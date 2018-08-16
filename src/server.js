575112569a390466887ea8dccc78c7042c160d83
clintfidel <clintfidel@gmail.com>
2018-08-16 13:26:51 +0100

    implement testing using jest

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config';
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(webpackMiddleware(webpack(webpackConfig)));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
